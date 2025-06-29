6d9bfbfa6e3bcbe77fbb6e9a7b0b757d43cf5853
Sanjeev Thapa <116951750+sanjeevlcc@users.noreply.github.com>
2025-06-29 22:50:13 +0545

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
   
   <div class="container">
-    <h1>Issued ver 1.2.334 </h1>
+    <h1>Issued ver 1.2.335 </h1>
     <h1>🏨 Welcome to Makalu Hotel Booking</h1>
     <form action="submit.php" method="POST" onsubmit="return validateForm()">
       <label for="name">Full Name</label>
